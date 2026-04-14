11eb328faf249ee0ca164498900e00f24b72d2d9
LV860 <plays867yer@gmail.com>
2026-04-14 14:54:54 -0500

    Feat: Cambio saludo dependiendo de la hora

--- Medix/app/src/main/java/com/example/medix/presentation/ui/components/schedule/GreetingSection.kt
+++ Medix/app/src/main/java/com/example/medix/presentation/ui/components/schedule/GreetingSection.kt
@@ -1,16 +1,18 @@
 package com.example.medix.presentation.ui.components.schedule
 
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import java.time.LocalTime
 
 @Composable
 fun GreetingSection() {
+
+    val greeting = getGreetingByTime()
+
     Column {
         Text(
             text = "Hola de nuevo",
@@ -19,8 +21,19 @@ fun GreetingSection() {
         )
 
         Text(
-            text = "Buenos días",
+            text = greeting,
             style = MaterialTheme.typography.headlineSmall
         )
+    }
+}
+
+private fun getGreetingByTime(): String {
+    val hour = LocalTime.now().hour
+
+    return when (hour) {
+        in 5..11 -> "Buenos días"
+        in 12..17 -> "Buenas tardes"
+        in 18..22 -> "Buenas noches"
+        else -> "Buenas madrugadas"
     }
 }
