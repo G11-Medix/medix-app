b314b5b2ca5c50da500af51b9f018e6effb768a8
LV860 <plays867yer@gmail.com>
2026-05-18 20:45:13 -0500

    Fix: Creacion canal notificacion

--- Medix/app/src/main/java/com/example/medix/MainActivity.kt
+++ Medix/app/src/main/java/com/example/medix/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.medix
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.medix.presentation.ui.navigation.NavGraph
 import com.example.medix.presentation.ui.theme.MedixTheme
-import org.osmdroid.config.Configuration
 import dagger.hilt.android.AndroidEntryPoint
+import org.osmdroid.config.Configuration
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createNotificationChannel()
 
         Configuration.getInstance().load(
             applicationContext,
@@ -23,6 +29,24 @@ class MainActivity : ComponentActivity() {
             MedixTheme {
                 NavGraph()
             }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                "medix_channel",
+                "Medix Notifications",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.description = "Canal de notificaciones"
+
+            val notificationManager =
+                getSystemService(NotificationManager::class.java)
+
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
